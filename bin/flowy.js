0247d77c692719e11e48f77230f8f2a1cd067c59
Russell Hanneken <rhanneken@pobox.com>
2026-05-24 17:43:03 -0400

    chore: project scaffold

---
+++ bin/flowy.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+'use strict';
+
+// Entry point — wired up fully in Task 7
+console.log('flowy ok');
---
+++ src/exitCodes.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+  SUCCESS: 0,
+  MIGRATION_FAILED: 1,
+  CONFIG_ERROR: 2,
+  HISTORY_STORE_ERROR: 3,
+};
